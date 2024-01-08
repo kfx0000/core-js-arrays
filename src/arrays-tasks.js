40a77ac4d1afd8ce0c13121af4b8c2c9a7431486
Andy <kfx0000@gmail.com>
2024-01-08 13:39:42 +0300

    feat: add 48 solutions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -423,8 +423,11 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce((a, e, i) => {
+    if (e % 2) a.push(i);
+    return a;
+  }, []);
 }
 
 /**
@@ -524,8 +527,8 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  return arr.slice(-n).concat(arr.slice(0, -n));
 }
 
 /**
